8fb7374618b8bf3da5d1a4e87c3795d2e53f39d6
Chalo Salvador <chalo@monogram.io>
2024-10-22 20:19:47 +0200

    Update next.config: images.remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,11 @@ import rewrites from './rewrites.js'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['images.unsplash.com'],
+		remotePatterns: [
+			{
+				hostname: 'images.unsplash.com',
+			},
+		],
 	},
 
 	experimental: {
